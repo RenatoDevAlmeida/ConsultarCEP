c3ffd204aef428edef9ca3360c519d8d82430c0b
Renato Almeida <renatoalvesalmeida2021@gmail.com>
2023-09-12 20:42:58 -0300

    mudança: ajustando responsividade e cores após a requisição.

--- js/script.js
+++ js/script.js
@@ -11,20 +11,22 @@ function consultaEndereco() {
         mensagemEndereco.style.color = "#838383";
         let url = `https://viacep.com.br/ws/${cep}/json/`;
         fetch(url).then(function(response){
-            response.json().then(function(data){    
+            response.json().then(function(data){  
+                 
                 if(data.complemento) {    
                 
                     mensagemEndereco.innerHTML = `  <h6>Endereço consultado:</h6> 
-                                                <p>CEP: <span> ${data.cep}</span> - 
-                                                Logradouro: <span> ${data.logradouro}</span> </p>
+                                                <p>CEP: <span> ${data.cep}</span></p>  
+                                                <p>Logradouro: <span> ${data.logradouro}</span> </p>
                                                 <p> Complemento:  <span>${data.complemento}</span> </p>
-                                                <p>Bairro:  <span>${data.bairro}</span> - Cidade: <span> ${data.localidade} - </span> <span>${data.uf} </span></p>`;
+                                                <p>Bairro:  <span>${data.bairro}</span> </p>
+                                                <p> Cidade: <span> ${data.localidade} - </span> <span>${data.uf} </span></p>`;
                     } else {
-                        mensagemEndereco.innerHTML = `  <h6>Endereço consultado:</h6> 
-                                                <p>CEP: <span> ${data.cep}</span> - 
-                                                Logradouro: <span> ${data.logradouro}</span> </p>
-                                                
-                                                <p>Bairro:  <span>${data.bairro}</span> - Cidade: <span> ${data.localidade} - </span> <span>${data.uf} </span></p>`;
+                        mensagemEndereco.innerHTML =`   <h6>Endereço consultado:</h6> 
+                                                        <p>CEP: <span> ${data.cep}</span></p>  
+                                                        <p>Logradouro: <span> ${data.logradouro}</span> </p>                                                        
+                                                        <p>Bairro:  <span>${data.bairro}</span> </p>
+                                                        <p> Cidade: <span> ${data.localidade} - </span> <span>${data.uf} </span></p>`;
                     }
 
         });
